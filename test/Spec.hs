059bf77a129822a52af0f009d460295f5133de71
Tim Habermaas <tim.habermaas@gmail.com>
2018-08-01 20:25:39 +0200

    Setup travis/tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,20 +1,38 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib (app)
+import Lib (app, AdminPassword(..))
 import Test.Hspec
 import Test.Hspec.Wai
-import Test.Hspec.Wai.JSON
+import qualified Data.Text.Lazy.Encoding as TE
+import qualified Data.Text.Lazy as T
+import Data.Semigroup ((<>))
+
+import qualified Db as Db
+import System.Environment (getEnv)
 
 main :: IO ()
-main = hspec spec
+main = do
+    dbUrl <- getEnv "DATABASE_URL"
+    conn <- Db.connect dbUrl
+    Db.migrate conn
+    hspec $ spec conn (AdminPassword "admin")
 
-spec :: Spec
-spec = with (return app) $ do
-    describe "GET /users" $ do
+spec :: Db.Connection -> AdminPassword -> Spec
+spec conn pw = with (return $ app conn pw) $ do
+    describe "GET /" $ do
         it "responds with 200" $ do
-            get "/users" `shouldRespondWith` 200
-        it "responds with [User]" $ do
-            let users = "[{\"userId\":1,\"userFirstName\":\"Isaac\",\"userLastName\":\"Newton\"},{\"userId\":2,\"userFirstName\":\"Albert\",\"userLastName\":\"Einstein\"}]"
-            get "/users" `shouldRespondWith` users
+            get "/" `shouldRespondWith` 200
+        it "has some FAQ entries" $ do
+            get "/" `shouldRespondWith` (successAndContains "Warum muss ich mich dieses Jahr überhaupt anmelden?")
+
+successAndContains :: T.Text -> ResponseMatcher
+successAndContains text =
+    200 { matchBody = MatchBody matcher }
+  where
+    matcher _header body =
+        if T.isInfixOf text (TE.decodeUtf8 body)
+            then Nothing
+            else Just $ show text <> " not found"
